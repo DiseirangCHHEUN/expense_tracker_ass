7c8aa12a3aa8c870a06436f11a37bb6f5128bc36
DiseirangCHHEUN <cseirang27@gmail.com>
2024-06-10 21:02:07 +0700

    Update auth_controller.dart

--- lib/views/auth/controller/auth_controller.dart
+++ lib/views/auth/controller/auth_controller.dart
@@ -2,7 +2,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 
 class AuthController extends GetxController {
